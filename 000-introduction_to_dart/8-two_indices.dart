1f336d5d3872dc6dffaed0d0e6cd1c5061175c28
Aaron Maeda <aaronmaeda7311@gmail.com>
2023-11-30 03:12:55 +0300

    two indices

--- 000-introduction_to_dart/8-two_indices.dart
+++ 000-introduction_to_dart/8-two_indices.dart
@@ -1,23 +1,23 @@
 List<int> findTwoNumbersWithSum(List<int> numbers, int target) {
-  Map<int, int> indeces = {};
+  Map<int, int> indices = {};
   for (int i = 0; i < numbers.length; i++) {
     int complement = target - numbers[i];
 
-    if (indeces.containsKey(complement)) {
-      return [complement, numbers[i]];
+    if (indices.containsKey(complement)) {
+      return [indices[complement]!, i];
     }
 
-    indeces[numbers[i]] = i;
+    indices[numbers[i]] = i;
   }
   return [];
 }
 
 void main() {
-  List<int> numbers = [1, 2, 3];
-  int target = 4;
+  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  int target = 25;
   List<int> result = findTwoNumbersWithSum(numbers, target);
   if (result.isNotEmpty) {
-    print('${result[0]} and ${result[1]}');
+    print('$result');
   } else {
     print('no results found');
   }
