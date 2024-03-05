5f5e6d8d4c9bb4b8d360bfc837043103aacfaa8e
Aaron Maeda <60480047+AaronBnras@users.noreply.github.com>
2024-03-05 21:38:38 +0300

    initial commit

--- 002-BMI_calculator/lib/main.dart
+++ 002-BMI_calculator/lib/main.dart
@@ -1,89 +1,191 @@
 import 'package:flutter/material.dart';
+import 'components/image_banner.dart';
+import 'model/calculator.dart';
 
 void main() {
-  runApp(MyCalculator());
+  runApp(const MyApp());
 }
 
-class MyCalculator extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      title: 'BMI Calculator',
       theme: ThemeData(
-        primaryColor: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        useMaterial3: true,
       ),
-      home: Calculator(),
+      home: const MyHomePage(title: 'Home'),
     );
   }
 }
 
-class Calculator extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
   @override
-  _CalculatorState createState() => _CalculatorState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _CalculatorState extends State<Calculator> {
-  final TextEditingController _heightController = TextEditingController();
-  final TextEditingController _weightController = TextEditingController();
-  double? _result;
+class _MyHomePageState extends State<MyHomePage> {
+  BMICalculator myCalculator = BMICalculator.empty();
+  final _weightController = TextEditingController();
+  final _heightController = TextEditingController();
+  String _remark = "Please enter your weight and height to calculate your BMI";
+  String _bmi = "-";
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('BMI Calculator'),
-        centerTitle: true,
-        backgroundColor: Colors.pink,
-      ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              controller: _heightController,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                labelText: 'Height in cm',
-                icon: Icon(Icons.trending_up),
+        body: Column(children: [
+          Stack(children: <Widget>[
+            const ImageBanner("assets/images/background.png"),
+            Center(
+                child: Column(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 64, 0, 0),
+                      child: Text(
+                        "Your BMI is:",
+                        style: TextStyle(color: Colors.white, fontSize: 24),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsetsDirectional.symmetric(vertical: 12),
+                      child: Text(
+                        _bmi,
+                        style: const TextStyle(color: Colors.white, fontSize: 24),
+                      ),
+                    ),
+                    Text(
+                      _remark,
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                          fontStyle: FontStyle.italic),
+                    ),
+                  ],
+                ))
+          ]),
+          Padding(
+              padding: const EdgeInsetsDirectional.symmetric(
+                  horizontal: 16, vertical: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    controller: _weightController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(labelText: "Weight (Kg)"),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    controller: _heightController,
+                    decoration: const InputDecoration(labelText: "Height (m)"),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.redAccent,
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4))),
+                    onPressed: () {
+                      String? validationError;
+                      if (_weightController.text.isEmpty ||
+                          _heightController.text.isEmpty) {
+                        validationError = "Please provide your weight and height";
+                      }
+
+                      if (validationError != null) {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              backgroundColor: Colors.white,
+                              surfaceTintColor: Colors.transparent,
+                              title: const Text("Error"),
+                              content: Text(validationError!),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop(); // Close the dialog
+                                  },
+                                  child: const Text("OK"),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      } else {
+                        myCalculator.height = double.tryParse(_heightController.text)!;
+                        myCalculator.weight = double.tryParse(_weightController.text)!;
+                        myCalculator.calculateBMI();
+
+                        setState(() {
+                          _remark = myCalculator.getRemark();
+                          _bmi = myCalculator.getCalculatedBMI()!;
+                        });
+
+                      }
+                    },
+                    child: const Text("CALCULATE"),
+                  ),
+                ],
+              )),
+          const Expanded(
+            child: Center(
+              child: SizedBox(
+                height: 20,
               ),
             ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _weightController,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                labelText: 'Weight in KG',
-                icon: Icon(Icons.line_weight),
-              ),
-            ),
-            const SizedBox(height: 15),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.pinkAccent,
-              ),
-              child: const Text('Calculate', style: TextStyle(color: Colors.white)),
-              onPressed: () {
-                setState(() {
-                  _calculateBMI();
-                });
-              },
-            ),
-            const SizedBox(height: 15),
-            Text(
-              _result != null ? 'BMI: ${_result?.toStringAsFixed(2)}' : 'Enter your details',
-              style: const TextStyle(color: Colors.redAccent, fontSize: 19.4, fontWeight: FontWeight.w500),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  void _calculateBMI() {
-    double height = double.parse(_heightController.text) / 100; // Convert cm to meters
-    double weight = double.parse(_weightController.text);
-
-    _result = weight / (height * height);// Correct BMI formula
-
+          ),
+          const Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                  padding:
+                  EdgeInsetsDirectional.symmetric(horizontal: 16, vertical: 16),
+                  child: Column(
+                    children: [
+                      Center(
+                        child: Text(
+                          "Body mass index, or BMI, is used to determine whether "
+                              "you are in a healthy weight range for your height",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.black54),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "* This calculator shouldn't be used for pregnant women or children",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black38,
+                            fontStyle: FontStyle.italic),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                    ],
+                  ))
+            /**/
+          )
+        ]));
   }
 }
